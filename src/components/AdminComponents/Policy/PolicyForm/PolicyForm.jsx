cb8b699578d28c4a4e641387baf83676d78a00b5
carloslorenzo7 <carlos__lorenzo@hotmail.com>
2024-11-20 11:39:10 -0300

    actualizacion

--- src/components/AdminComponents/Policy/PolicyForm/PolicyForm.jsx
+++ src/components/AdminComponents/Policy/PolicyForm/PolicyForm.jsx
@@ -172,7 +172,7 @@ const PolicyForm = () => {
             {...register("tipoDeSeguro", {
               required: "El tipo de seguro es obligatorio",
             })}
-            //onChange={(e) => setTipoDeSeguro(e.target.value)}
+            
             className="appearance-none block w-full px-3 py-2 border text-gray-900 border-gray-300 bg-white rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
           >
             <option
